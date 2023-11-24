613361e991d3b296fc8c9bd510bd5ec69a21f535
Inês Barbosa <93823221+MInesBarbosaa@users.noreply.github.com>
2023-11-24 14:58:55 +0000

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,15 +1,8 @@
 const addButton = document.getElementById("add");
 const tasks = document.getElementById("tasks");
 
-const items = [];
 const items = getItems();
 
-// listener for a click or keypress
-
-taskInput.addEventListener("keyup", processKeyPress);
-addButton.addEventListener("click", addNewItem);
-
-// When the buttons is clicked or something is tipping in the input, the functions defined inside the listeners are run
 items.forEach(item => {
     tasks.appendChild(CreateElementForTask(item));
 });
@@ -18,13 +11,7 @@ addButton.addEventListener('click', addNewItem);
 taskInput.addEventListener('keyup', processKeyPress);
 
 function processKeyPress(event) {
-    // console.log(event.key);   this allows to obtain only the letters that were tipped, instead of other informations
-
-    // Remove the disable from the Add button if soemthing is tipped
-    addButton.disabled = event.target.value.trim() === ""; // Verify if this is empty or not
     addButton.disabled = event.target.value.trim() === "";
-
-    // console.log(event.target.value.trim === ""); 
 
     if (event.key === "Enter") {
     if (event.key === "Enter" && event.target.value.trim() != "") {
@@ -32,21 +19,9 @@ function processKeyPress(event) {
     }
 }
 
-//const items = [
-    // Test Items
-    //{ value: "my item", complete: false },
-    //{ value: "my item 2", complete: false },
-//];
-
-//for (let item of items) {
-//    const li = createElementForTask(item);
-//    tasks.appendChild(li);
-//}
-
-
 function createElementForTask(task) {
     console.log(task);
-    // Create new List items
+
     const template = document.getElementById("taskTemplate");
     const newListItem = template.content.cloneNode(true);
 
@@ -75,32 +50,13 @@ function addNewItem() {
     items.push(task);
     saveItems();
 
-function addNewItem() {
-   // console.log(taskInput.value);  gives what is put in the input when add is pressed
-
-   const task = {
-       value: taskInput.value,
-       complete: false
-   } // creates an array 
     let newItem = createElementForTask(task);
     tasks.appendChild(newItem);
 
-   items.push(task);
     taskInput.value = "";
     taskInput.focus();
 }
 
-//checkbox.onchange = function (event) {
-//    item.complete = event.target.checked;
-//    saveItems();
-//};
-
-   let newItem = createElementForTask(task);
-   //tasks.appendChild(newItem);
-
-   taskInput.value = "";
-   taskInput.focus();
-//get data out of local storage
 function getItems() {
     const noItemsFound = "[]";
     const itemsJSON = localStorage.getItem('items') || noItemsFound;
